5f038722f0e4c8ffe898b7d6e00df34a7b6dbc3c
oki-aryawan <sandikalakual@gmail.com>
2022-04-06 12:50:14 +0800

    creating gui

---
+++ GUI/GUI.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import QPixmap
+import design
+
+
+class MainApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.update_label_object)
+
+    def update_label_object(self):
+        self.label.setText('Processing....')
+        image_path = \
+            QtWidgets.QFileDialog.getOpenFileName(self, 'Choose imageto open',
+                                                  '.', '*.png *.jpg *.bmp')
+        print(type(image_path))
+        print(image_path[0])
+        print(image_path[1])
+
+        image_path = image_path[0]
+        pixmap_image = QPixmap(image_path)
+        self.label.setPixmap(pixmap_image)
+        self.label.resize(pixmap_image.width(), pixmap_image.height())
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
